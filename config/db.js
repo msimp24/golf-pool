20e29b606331295e44828e2dfb7f470a6c63e85a
Mark Simpson <mark.simpson4@gmail.com>
2025-02-17 16:51:19 +0000

    test db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3')
 
-let db = new sqlite3.Database(process.env.DB_URL, (err) => {
+let db = new sqlite3.Database('pga-data.db', (err) => {
   if (err) {
     console.log('Error occured' + err.message)
   } else {
