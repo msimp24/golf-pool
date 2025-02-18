d3ba56ef9eee82268a1d8b3dc60dbcf1410a1d3c
Mark Simpson <mark.simpson4@gmail.com>
2025-02-18 17:22:22 +0000

    fixed config

--- config/db.js
+++ config/db.js
@@ -11,12 +11,14 @@ const prodPath = '/var/www/golf-pool'
 if (fs.existsSync(prodPath)) {
   environment = 'prod'
   DB_PATH = path.join(prodPath, 'pga-data-scraper', 'pga-data.db')
+  console.log('In prod')
 } else {
   environment = 'dev'
   DB_PATH = process.env.DB_URL
+  console.log('in dev')
 }
 
-let db = new sqlite3.Database(process.env.DB_URL, (err) => {
+let db = new sqlite3.Database(DB_PATH, (err) => {
   if (err) {
     console.log('Error occured' + err.message)
   } else {
