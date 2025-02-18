2b1e546f145404ff69fa0cb654767aa23d9300e6
Mark Simpson <mark.simpson4@gmail.com>
2025-02-18 16:54:38 +0000

    change prod path

--- config/db.js
+++ config/db.js
@@ -1,5 +1,21 @@
 const sqlite3 = require('sqlite3')
 
+const fs = require('fs')
+const path = require('path')
+
+let environment
+let DB_PATH
+
+const prodPath = '/var/www/golf-pool'
+
+if (fs.existsSync(prodPath)) {
+  environment = 'prod'
+  DB_PATH = path.join(prodPath, 'pga-data-scraper', 'pga-data.db')
+} else {
+  environment = 'dev'
+  DB_PATH = process.env.DB_URL
+}
+
 let db = new sqlite3.Database(process.env.DB_URL, (err) => {
   if (err) {
     console.log('Error occured' + err.message)
