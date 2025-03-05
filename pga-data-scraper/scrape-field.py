6444c4cc528e23b36aabd9dbeba5942531c6c919
Mark Simpson <mark.simpson4@gmail.com>
2025-03-05 14:33:44 +0000

    troubleshooting

--- pga-data-scraper/scrape-field.py
+++ pga-data-scraper/scrape-field.py
@@ -14,6 +14,7 @@ else:
 conn = sqlite3.connect(DB_PATH)
 mycursor = conn.cursor()
 
+print(environment)
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
